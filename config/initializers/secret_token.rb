2b1893db48bf2ef95ac797c866cbd6689798181d
Qualquer Valor <qualquervalor@gmail.com>
2013-12-04 13:55:19 -0600

    created rails app threesquare with hirb included

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Threesquare::Application.config.secret_key_base = '7f78529166722aca63162d6373f81021a9c84230b6db4fa6716f98a98154fb4463b20c979f49ca5fa21b9763f82f6d3f2043650276c7668128c770ba3793a373'
